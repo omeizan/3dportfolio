09eaef139d0fa9d253cbd7e82a8f5a070a408ad9
omeizan <omeizanoel@gmail.com>
2022-05-26 18:28:41 -0500

    Deleted Test files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Spline from '@splinetool/react-spline'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className ="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20" >
+        {/*NavBar*/}
+        <nav className = "w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
+          <div className="w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center">
+            <p className='txt-lg text-slate-200 font-medium'>NOEL</p>
+
+            <div className = "hidden md:flex items-center gap-6 ml-6 flex-1">
+              
+            </div>
+          </div>
+        </nav>
+        <div className = "relative" id ="home">
+          <Spline scene="https://prod.spline.design/OMi17k0PbFfnAKVc/scene.splinecode" />
+        </div>
+      </div>
+     
   );
 }
 
